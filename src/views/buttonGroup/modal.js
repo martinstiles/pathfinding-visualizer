819133dffc97a6ea415dafa1d246895246d07462
Martin Stiles <32123897+martinstiles@users.noreply.github.com>
2020-04-25 21:01:59 +0200

    Update modal

--- src/views/buttonGroup/modal.js
+++ src/views/buttonGroup/modal.js
@@ -48,18 +48,33 @@ const TransitionsModal = () => {
       >
         <Fade in={open}>
           <div style={paper}>
-            <h2 > Welcome to my Pathfinding Visualizer</h2>
-            <div style={{display: 'flex', flexDirection: 'row'}}>
-              <p style={{color: '#63C132'}}>Green cell&nbsp;</p>
-              <p> - start node </p>
+            <div style={{textAlign: 'center'}}>
+              <h2 > Welcome to my Pathfinding Visualizer</h2>
             </div>
-            <div style={{display: 'flex', flexDirection: 'row'}}>
-              <p style={{color: '#cf2e2e'}}>Red cell&nbsp;</p>
-              <p> - goal node </p>
+            <div style={{display: 'flex', justifyContent: 'space-between', marginLeft: '1em', marginRight: '1em'}}>
+              <div style={{display: 'flex', flexDirection: 'row'}}>
+                <p style={{color: '#63C132'}}> Green cell&nbsp; </p>
+                <p> - start node </p>
+              </div>
+              <div style={{display: 'flex', flexDirection: 'row'}}>
+                <p style={{color: '#2d749a'}}> Blue cell&nbsp; </p>
+                <p> - expanded node </p>
+              </div>
+            </div>
+
+            <div style={{display: 'flex', justifyContent: 'space-between', marginLeft: '1em', marginRight: '1em'}}>
+              <div style={{display: 'flex', flexDirection: 'row'}}>
+                <p style={{color: '#cf2e2e'}}> Red cell&nbsp; </p>
+                <p> - goal node </p>
+              </div>
+              <div style={{display: 'flex', flexDirection: 'row'}}>
+                <p style={{color: '#ffff60'}}> Yellow cell&nbsp; </p>
+                <p> - shortest path </p>
+              </div>
             </div>
             <p> 1. Click inside the grid and hold down your mouse button to draw walls </p>
             <p> 2. Clicking again will allow you to remove walls </p>
-            <p> 3. Select algorithm of choice (Currently only Dijkstra :D) </p>
+            <p> 3. Select algorithm of choice </p>
             <p> 4. Select a different speed if you feel like it </p>
             <p> 5. Hit play :)  </p>
             <div style={{textAlign: 'center'}}>
